31f6f3631927e3ee1974cf94b36b9cb136e8d22c
Mike Bryant <m@ocado.com>
2016-08-02 21:14:00 +0100

    Add the pod termination

--- chaos.py
+++ chaos.py
@@ -3,6 +3,7 @@
 import logging
 import os
 import pykube
+import random
 import time
 
 KILL_FREQUENCY = int(os.environ.get('CHAOS_MONKEY_KILL_FREQUENCY_SECONDS', 300))
@@ -13,8 +14,13 @@ LOGGER = logging.getLogger(__name__)
 api = pykube.HTTPClient(pykube.KubeConfig.from_service_account())
 
 while True:
-    pods = pykube.Pod.objects(api)
-    print(str(pods))
-    print(str(list(pods)))
+    namespaces = pykube.Namespace.objects(api)
+
+    pods = []
+    for namespace in namespaces:
+        pods.extend(list(pykube.Pod.objects(api).filter(namespace=namespace.name)))
+    pod = random.choice(pods)
+    LOGGER.info("Terminating pod %s/%s", pod.namespace, pod.name)
+    pod.delete()
 
     time.sleep(KILL_FREQUENCY)
